aba06aa4126f24887b9548a799f404d9caf7cd8e
Emanuel S <105735869+Emanuel-Sifuentes@users.noreply.github.com>
2022-05-27 17:26:16 -0400

    fixed reference

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/salrashid123/terraform-provider-http-full/internal/provider"
+	"github.com/Emanuel-Sifuentes/terraform-provider-http-full/internal/provider"
 )
 
 func main() {
